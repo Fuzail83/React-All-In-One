086e2107d57e4abb8c4273e7ffb21e197025c744
Fuzail Akhtar <FAkhtar@texala.net>
2023-07-11 10:23:46 +0530

    add error page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,9 @@ const Home = () => {
   return (
     <>
       <Helmet title="Homepage" />
-      <div className="home-container">
-        {/* <h2> Home Page</h2> */}
-        <p>
+      {/* <div className="home-container"> */}
+      {/* <h2> Home Page</h2> */}
+      {/* <p>
           As a React developer, you are in the right place to showcase your
           skills and build amazing web applications. React is a popular
           JavaScript library for building user interfaces, known for its
@@ -24,9 +24,44 @@ const Home = () => {
         <p>
           Take advantage of the rich React ecosystem, including libraries,
           frameworks, and tools that enhance your development experience. Keep
-          exploring and building awesome projects!
-          {/* <GoogleAddress /> */}
-        </p>
+          exploring and building awesome projects! */}
+      {/* <GoogleAddress /> */}
+      {/* </p> */}
+      {/* </div> */}
+      <div className="home-container">
+        <header>
+          <h1>Welcome to My Website</h1>
+        </header>
+        <section className="hero-section">
+          <div className="hero-content">
+            <h2>Discover the World of React</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              vehicula tincidunt leo, vel consectetur odio convallis eu.
+            </p>
+            <button className="cta-button">Get Started</button>
+          </div>
+          <div className="hero-image">
+            <img src="/images/hero.jpg" alt="Hero" />
+          </div>
+        </section>
+        <section className="features-section">
+          <div className="feature">
+            <img src="/images/feature1.jpg" alt="Feature" />
+            <h3>Easy to Use</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+          <div className="feature">
+            <img src="/images/feature2.jpg" alt="Feature" />
+            <h3>Flexible and Scalable</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+          <div className="feature">
+            <img src="/images/feature3.jpg" alt="Feature" />
+            <h3>High Performance</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </section>
       </div>
     </>
   );
---
+++ src/components/common/ErrorPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../assets/css/Error.css";
+import { Helmet } from "react-helmet";
+
+const ErrorPage = () => {
+  return (
+    <>
+      <Helmet title="Page Not Found" />
+      <div className="error-container">
+        <div className="error-content">
+          <h1 className="error-title">Oops!</h1>
+          <p className="error-message">
+            We're sorry, but something went wrong.
+          </p>
+          <p className="error-message">
+            Please try again later or contact support.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ErrorPage;
